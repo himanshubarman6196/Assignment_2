b4afbc9afa2fa888e880f5cb8cd154f0fd88ee77
himanshubarman <himanshubarman43@gmail.com>
2021-04-20 16:34:18 +0530

    updated setting.py

--- Assignment/Assignment/settings.py
+++ Assignment/Assignment/settings.py
@@ -126,6 +126,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
