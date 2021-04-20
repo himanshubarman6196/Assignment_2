b70bc012861d5eb388b769c356a50123bf1fc74a
himanshubarman <himanshubarman43@gmail.com>
2021-04-20 17:09:52 +0530

    updated project deployed

--- Assignment/Assignment/settings.py
+++ Assignment/Assignment/settings.py
@@ -126,7 +126,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
